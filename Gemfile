76b0ad4611e8fc72b6fba85edfdadd3dca42de86
OTA Daichi <daichi.ota@access-company.com>
2013-12-19 16:14:36 +0900

    Enable to add product to cart

--- Gemfile
+++ Gemfile
@@ -43,3 +43,7 @@ end
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+
+gem 'quiet_assets', :group => :development
+
+
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,18 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
   validates :title, :description, :price, presence: true
   validates :price, numericality: { only_integer: true }
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      true
+    else
+      errors.add(:base, 'Line Items present')
+      false
+    end
+  end
 end

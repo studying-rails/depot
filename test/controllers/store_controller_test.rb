8d5d35a4238efadf6ee95473ff2f753c6809956c
OTA Daichi <daichi.ota@access-company.com>
2013-12-19 15:31:13 +0900

    Show as currency in view

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,8 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select 'h3',     'MyString'
+    assert_select '.price', /\$[\d,]+\.\d{2}/
   end
 
 end
